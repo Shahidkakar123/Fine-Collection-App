75408efc934e75448d147079a92390a88dc06cc6
Shahid Khan <skhan@teresol.com>
2025-08-13 12:25:43 +0500

    Update backend to use mongodb_uri

--- backend/index.js
+++ backend/index.js
@@ -27,6 +27,6 @@ mongoose
 app.use("/api/users", userRoutes);
 app.use("/api/items", itemRoutes);
 
-app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
+app.listen(process.env.PORT || 3000, () => console.log(`Server running on port ${process.env.PORT || 3000}`));
 
 module.exports = app;
