7ef56f877f276ec784594c74b12a8fa5854ec427
Shahid Khan <skhan@teresol.com>
2025-08-18 12:02:13 +0500

    minor

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.get("/", (req, res) => {
 });
 
 mongoose
-  .connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGODB_URI)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
